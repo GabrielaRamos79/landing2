e03cf83af1be60029a0aa77e9ca164397bf87535
GabrielaRamos79 <gabyramos@me.com>
2024-10-07 12:56:55 +0200

    Primer commit de la landing2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Carousel from "./components/Carousel/"
+import Header from "./components/Header";
+
 
 function App() {
   return (
