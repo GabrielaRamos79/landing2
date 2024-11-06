932e134fb00760cac5753311fa117f372075ec45
GabrielaRamos79 <gabyramos@me.com>
2024-11-06 13:54:52 +0100

    Fixing mensaje enviado3

--- src/components/MensajeEnviado/MensajeEnviado.js
+++ src/components/MensajeEnviado/MensajeEnviado.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './MensajeEnviado.css';  // Estilos personalizados
+import Contact from '../Contact/Contact';
+import Footer from '../Footer/Footer';
 import trazo from "../../images/details/trazado.png";
 import gif1 from "../../images/details/gif1.gif";
 
@@ -9,6 +11,8 @@ const MensajeEnviado = () => {
       <h1>¡Mensaje Recibido!</h1>
       <p>Pronto contactaremos contigo para darle caña a tu marca.</p>
       <img src="ruta-al-gif.gif" alt="Gif confirmación" />
+      <Contact />
+      <Footer />
     </div>
   );
 };
